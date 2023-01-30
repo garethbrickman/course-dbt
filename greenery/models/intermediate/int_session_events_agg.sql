f2fe22ad76517907f04ad6e883f63b5c21ab5311
Gareth Brickman <gbrickman@pm.me>
2023-01-30 06:22:35 +0000

    refactor int_session_events_agg to use macro

--- greenery/models/intermediate/int_session_events_agg.sql
+++ greenery/models/intermediate/int_session_events_agg.sql
@@ -8,10 +8,7 @@ with session_events as (
   select
     user_guid
     , session_guid
-    , sum(case when event_type = 'add_to_cart' then 1 else 0 end) as add_to_carts
-    , sum(case when event_type = 'checkout' then 1 else 0 end) as checkouts
-    , sum(case when event_type = 'package_shipped' then 1 else 0 end) as package_shippeds
-    , sum(case when event_type = 'page_view' then 1 else 0 end) as page_views
+    {{ make_event_types_with_counts() }}
     , min(created_at_utc) as first_session_event_at_utc
     , max(created_at_utc) as last_session_event_at_utc
   from session_events
